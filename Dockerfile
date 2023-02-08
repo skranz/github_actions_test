6f2617ce7365c6990663a2388147eb218454b5a0
Sebastian Kranz <sebkranz@gmail.com>
2023-02-08 10:44:29 +0100

    Update

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,7 @@ RUN echo "Ich bin ein neuer Text" > /root/mytext.txt
 
 RUN echo $MYSECRET > /root/MYSECRET.txt
 
+RUN echo $MYVAR > /root/myvar.txt
+
+
 MAINTAINER Sebastian Kranz "sebastian.kranz@uni-ulm.de"
--- scripts/run_test.R
+++ scripts/run_test.R
@@ -5,7 +5,12 @@ writeLines("I am a text","~/output/test.txt")
 txt = readLines("~/mytext.txt")
 cat("\n", txt,"\n")
 
+myvar <- Sys.getenv("MYVAR")
+cat("\nSys.getenv: ", myvar)
+
+secret = readLines("~/MYVAR.txt")
+cat("\nMYVAR.txt: ", secret,"\n")
 
 # Read and show Github secret
 secret = readLines("~/MYSECRET.txt")
-cat("\n", secret,"\n")
+cat("\n MYSECRET.txt: ", secret,"\n")
