ce0cf1fbce344c3cdd95d586bff3d695d9c84d60
Sebastian Kranz <sebkranz@gmail.com>
2023-02-08 10:02:17 +0100

    Updates

--- scripts/run_test.R
+++ scripts/run_test.R
@@ -1,3 +1,8 @@
 cat("\nHello World!\n")
 
 writeLines("I am a text","~/output/test.txt")
+
+# Read and show Github secret
+secret.file = "/run/secrets/MYSECRET"
+secret = readLines(secret.file)
+cat("\n", secret,"\n")
